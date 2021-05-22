5f0bdfc9778398709f3d17b670b6af60de6b92ef
Tony_Xiao <39907973+qq4010118@users.noreply.github.com>
2021-05-22 17:15:00 +0800

    guess homework1: mail box layout

--- app/src/main/java/tw/com/batMobile/guess/MainActivity.kt
+++ app/src/main/java/tw/com/batMobile/guess/MainActivity.kt
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    val secretNumber = SecretNumber()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        R.id.hello_main
+        setContentView(R.layout.activity_box)
         R.drawable.androidparty
         R.color.message
         print(R.string.ok)
